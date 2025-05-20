146e974e8785d82b5eec6a0dc75aa496b96e3770
secile <secile.develop@hotmail.com>
2025-05-20 14:52:19 +0900

    send/receive data between ESP32 and micropad.
    * reverse TX and RX to match the micro:bit.
    * change device name start with 'BBC micro:bit' to be supported by micropad.

--- micropad_server_esp32.ino
+++ micropad_server_esp32.ino
@@ -1,41 +1,18 @@
 /*
-    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
-    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
-    Ported to Arduino ESP32 by Evandro Copercini
-
-   Create a BLE server that, once we receive a connection, will send periodic notifications.
-   The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
-   Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE"
-   Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send data with  "NOTIFY"
-
-   The design of creating the BLE server is:
-   1. Create a BLE Server
-   2. Create a BLE Service
-   3. Create a BLE Characteristic on the Service
-   4. Create a BLE Descriptor on the characteristic
-   5. Start the service.
-   6. Start advertising.
-
-   In this example rxValue is the data received (only accessible inside that function).
-   And txValue is the data to be sent, in this example just a byte incremented every second.
+  based on BLE UART example.
+  
+  Copyright (c) 2025 secile
+  This software is released under the MIT license.
+  see https://opensource.org/licenses/MIT
 */
+
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
-BLEServer *pServer = NULL;
 BLECharacteristic *pTxCharacteristic;
 bool deviceConnected = false;
-bool oldDeviceConnected = false;
-uint8_t txValue = 0;
-
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
-
-#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"  // UART service UUID
-#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
-#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
 class MyServerCallbacks : public BLEServerCallbacks {
   void onConnect(BLEServer *pServer) {
@@ -67,23 +44,46 @@ class MyCallbacks : public BLECharacteristicCallbacks {
 void setup() {
   Serial.begin(115200);
 
+  initBLE();
+}
+
+void initBLE() {
+  // +----------------------------+           +----------------------------+
+  // | [micro:bit, ESP32]         |           | [micropad on Smartphone]   |
+  // | as Peripheral.             |           | as Central.                |
+  // | do Advertise.              |           | do Scan.                   |
+  // | become Server.             |           | become Client.             |
+  // | expose Nordic UART Service.|           | connect to Service.        |
+  // | - TX (0002) [Nofity, CCCD] | --------> | - TX (0002) start notify.  |
+  // | - RX (0003) [Write]        | <-------- | - RX (0003) write value.   |
+  // +----------------------------+           +----------------------------+
+
+  // reverse TX and RX to match the micro:bit.
+  const char* UART_SERVICE_UUID      = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
+  const char* CHARACTERISTIC_UUID_TX = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E";
+  const char* CHARACTERISTIC_UUID_RX = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E";
+
+  // micro:pad only supports device name starts with "BBC micro:bit".
+  // if you change device name, you also have to change micro:pad source code below.
+  // await navigator.bluetooth.requestDevice({ filters: [{ namePrefix: "BBC micro:bit" }], ...
+  const char* BLE_DEVICE_NAME = "BBC micro:bit ESP32";
+
   // Create the BLE Device
-  BLEDevice::init("UART Service");
+  BLEDevice::init(BLE_DEVICE_NAME);
 
   // Create the BLE Server
-  pServer = BLEDevice::createServer();
+  BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
 
   // Create the BLE Service
-  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLEService *pService = pServer->createService(UART_SERVICE_UUID);
 
-  // Create a BLE Characteristic
+  // TX (0002) ESP32(server) -> micropad(client)
   pTxCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID_TX, BLECharacteristic::PROPERTY_NOTIFY);
-
   pTxCharacteristic->addDescriptor(new BLE2902());
-
+  
+  // RX (0003) ESP32(server) <- micropad(client)
   BLECharacteristic *pRxCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID_RX, BLECharacteristic::PROPERTY_WRITE);
-
   pRxCharacteristic->setCallbacks(new MyCallbacks());
 
   // Start the service
@@ -91,28 +91,10 @@ void setup() {
 
   // Start advertising
   pServer->getAdvertising()->start();
+
   Serial.println("Waiting a client connection to notify...");
 }
 
 void loop() {
-
-  if (deviceConnected) {
-    pTxCharacteristic->setValue(&txValue, 1);
-    pTxCharacteristic->notify();
-    txValue++;
-    delay(10);  // bluetooth stack will go into congestion, if too many packets are sent
-  }
-
-  // disconnecting
-  if (!deviceConnected && oldDeviceConnected) {
-    delay(500);                   // give the bluetooth stack the chance to get things ready
-    pServer->startAdvertising();  // restart advertising
-    Serial.println("start advertising");
-    oldDeviceConnected = deviceConnected;
-  }
-  // connecting
-  if (deviceConnected && !oldDeviceConnected) {
-    // do stuff here on connecting
-    oldDeviceConnected = deviceConnected;
-  }
+  delay(500);
 }
